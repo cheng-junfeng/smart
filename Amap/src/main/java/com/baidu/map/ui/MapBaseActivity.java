bd4c8e478fab60f221a09cec4c6489f3504731b5
chengjunfeng <sjun945@outlook.com>
2018-05-23 14:40:10 +0800

    百度地图：跟随生命周期+点击增加标记+控制缩进/移动/卫星/路况

--- Amap/src/main/java/com/baidu/map/ui/MapBaseActivity.java
+++ Amap/src/main/java/com/baidu/map/ui/MapBaseActivity.java
@@ -3,6 +3,8 @@ package com.baidu.map.ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -11,8 +13,14 @@ import com.baidu.location.LocationClientOption;
 import com.baidu.map.config.Const;
 import com.baidu.map.utils.BaiduMapUtils;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapPoi;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.UiSettings;
 import com.baidu.mapapi.model.LatLng;
 import com.baidu.track.R;
 import com.baidu.track.R2;
@@ -27,18 +35,35 @@ import butterknife.OnClick;
  * 地图基础应用
  * 2018-05-23
  */
-public class MapBaseActivity extends BmapBaseCompatActivity implements BDLocationListener{
+public class MapBaseActivity extends BmapBaseCompatActivity implements BDLocationListener {
 
     private final static String TAG = "MapBaseActivity";
     @BindView(R2.id.mapView)
     MapView mapView;
+    @BindView(R2.id.btn_zoom)
+    CheckBox btnZoom;
+    @BindView(R2.id.btn_other)
+    CheckBox btnOther;
+    @BindView(R2.id.btn_star)
+    CheckBox btnStar;
+    @BindView(R2.id.btn_traffic)
+    CheckBox btnTraffic;
 
     //控制器
     private BaiduMap mBaiduMap;
+    //UI手势控制器
+    private UiSettings mUiSettings;
     //定位对象
     private BDLocation curBDLocation;
     //定位客户端
     private LocationClient mLocClient;
+    //当前点击点
+    private LatLng currentPoint;
+    //标记
+    BitmapDescriptor tempMarker = BitmapDescriptorFactory
+            .fromResource(R.mipmap.icon_mark1);
+    BitmapDescriptor mLocationMarker = BitmapDescriptorFactory
+            .fromResource(R.mipmap.icon_point);
 
     private Context mContext;
     private boolean isRequestLocation;
@@ -58,13 +83,83 @@ public class MapBaseActivity extends BmapBaseCompatActivity implements BDLocatio
             }
         });
         init();
+        setListener();
     }
 
-    private void init(){
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // activity 恢复时同时恢复地图控件
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // activity 暂停时同时暂停地图控件
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // activity 销毁时同时销毁地图控件
+        mapView.onDestroy();
+    }
+
+    private void init() {
         mContext = this;
         mBaiduMap = mapView.getMap();
-        BaiduMapUtils.targetPoint(mBaiduMap,
-                new LatLng(Const.DEFAULT_CITY_LATITUDE, Const.DEFAULT_CITY_LONGITUDE), 16f);
+        mUiSettings = mBaiduMap.getUiSettings();
+        //不显示放大放小按钮
+        mapView.showZoomControls(false);
+        getLocation();
+    }
+
+    private void setListener() {
+        mBaiduMap.setOnMapClickListener(new BaiduMap.OnMapClickListener() {
+            public void onMapClick(LatLng point) {
+                currentPoint = point;
+                setMarkerView();
+                LogUtil.d(TAG, "单击地图");
+            }
+
+            public boolean onMapPoiClick(MapPoi poi) {
+                LogUtil.d(TAG, "单击地图Poi");
+                return false;
+            }
+        });
+        btnZoom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mUiSettings.setZoomGesturesEnabled(b);
+            }
+        });
+        btnOther.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                //移动
+                mUiSettings.setScrollGesturesEnabled(b);
+                //旋转
+                //mUiSettings.setRotateGesturesEnabled();
+            }
+        });
+        btnStar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                } else {
+                    mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                }
+            }
+        });
+        btnTraffic.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mBaiduMap.setTrafficEnabled(b);
+            }
+        });
     }
 
     @OnClick(R2.id.btn_location)
@@ -75,16 +170,18 @@ public class MapBaseActivity extends BmapBaseCompatActivity implements BDLocatio
 
     private void getLocation() {
         if (curBDLocation != null) {
-            BaiduMapUtils.targetPoint(mBaiduMap,
-                    new LatLng(curBDLocation.getLatitude(), curBDLocation.getLongitude()), 16f);
+            setLocationView(curBDLocation);
         } else {
             LogUtil.d(TAG, "getLocation");
             isRequestLocation = true;
-            initLocation();
+            startLocation();
+            curBDLocation = new BDLocation();
+            curBDLocation.setLatitude(Const.DEFAULT_CITY_LATITUDE);
+            curBDLocation.setLongitude(Const.DEFAULT_CITY_LONGITUDE);
         }
     }
 
-    private void initLocation() {
+    private void startLocation() {
         if (mLocClient == null) {
             mLocClient = new LocationClient(this);
             LocationClientOption option = new LocationClientOption();
@@ -101,20 +198,35 @@ public class MapBaseActivity extends BmapBaseCompatActivity implements BDLocatio
 
     @Override
     public void onReceiveLocation(BDLocation location) {
-        LogUtil.d(TAG, "onReceiveLocation:"+location);
+        setLocationView(location);
+    }
+
+    private void setLocationView(BDLocation location) {
+        LogUtil.d(TAG, "onReceiveLocation:" + location);
         if (location == null || mapView == null) {
             return;
         }
         curBDLocation = location;
-        LogUtil.d(TAG, "onReceiveLocation:"+location.getCity()+":"+location.getLatitude()+":"+location.getLongitude());
+        LogUtil.d(TAG, "onReceiveLocation:" + location.getLatitude() + ":" + location.getLongitude());
         MyLocationData data = new MyLocationData.Builder()
                 .accuracy(location.getRadius())
                 .direction(location.getDirection())
                 .latitude(location.getLatitude())
                 .longitude(location.getLongitude()).build();
         mBaiduMap.setMyLocationData(data);
-        if(isRequestLocation){
+        MyLocationConfiguration.LocationMode mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;
+        MarkerOptions locationMarker = new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).icon(mLocationMarker);
+        mBaiduMap.addOverlay(locationMarker);
+
+        MyLocationConfiguration config = new MyLocationConfiguration(mCurrentMode, true, mLocationMarker);
+        mBaiduMap.setMyLocationConfiguration(config);
+        if (isRequestLocation) {
             BaiduMapUtils.targetPoint(mBaiduMap, new LatLng(location.getLatitude(), location.getLongitude()), 16.0f);
         }
+    }
+
+    private void setMarkerView() {
+        MarkerOptions ooA = new MarkerOptions().position(currentPoint).icon(tempMarker);
+        mBaiduMap.addOverlay(ooA);
     }
 }
